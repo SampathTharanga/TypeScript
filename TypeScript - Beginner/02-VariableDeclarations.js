06decfa61eb0b9965dbf68f22dbe774cc3e324a5
Sampath Tharanga <sampaththaranga13@gmail.com>
2018-07-17 12:22:05 +0530

    TS file Compile

--- TypeScript - Beginner/02-VariableDeclarations.js
+++ TypeScript - Beginner/02-VariableDeclarations.js
@@ -4,6 +4,14 @@ REFERENCES:
     https://www.typescriptlang.org/docs/handbook/variable-declarations.html
 
 */
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
 //----------------- 01 - var declarations -----------------
 //Declaring a variable in JavaScript has always traditionally been done with "var" keyword.
 var a = 10;
@@ -176,3 +184,88 @@ var _loop_1 = function (i_3) {
 for (var i_3 = 0; i_3 < 10; i_3++) {
     _loop_1(i_3);
 }
+//---------------- 03 - "const" declarations ---------------------
+var numLivesForCat = 9;
+var kitty = {
+    name: "Aurora",
+    numLives: numLivesForCat
+};
+/*
+//Error
+kitty = {
+    name: "Danielle",
+    numLives: numLivesForCat
+};
+*/
+//all "okay"
+kitty.name = "Rory";
+kitty.name = "Kitty";
+kitty.name = "Cat";
+kitty.numLives--;
+//------------ Destructuring ----------------
+/*
+            Array Destructuring
+*/
+var input1 = [1, 2];
+var first = input1[0], second = input1[1];
+console.log(first); //Output 1
+console.log(second); //Output 2
+//You can create a variable for the remaining items in a list using the syntax ...
+var _a = [1, 2, 3, 4, 5], first1 = _a[0], rest = _a.slice(1);
+console.log(first); //Output 1
+console.log(rest); //Output [2, 3, 4, 5]
+/*
+        Object Destructuring
+*/
+var o1 = {
+    a1: "foo",
+    b1: 12,
+    c1: "bar"
+};
+var a1 = o1.a1, b1 = o1.b1;
+//Property renaming
+var newame1 = o1.a1, newName2 = o1.b1;
+//Default Values
+function keepWholeObject(wholeObject) {
+    var a = wholeObject.a, _a = wholeObject.b, b = _a === void 0 ? 1001 : _a;
+}
+function ff(_a) {
+    var a = _a.a, b = _a.b;
+    //...
+}
+function ff1(_a) {
+    var _b = _a === void 0 ? { a: "", b: 0 } : _a, a = _b.a, b = _b.b;
+    //...
+}
+ff1(); //ok default to { a: "", b: 0 }
+function ff2(_a) {
+    var _b = _a === void 0 ? { a: "" } : _a, a = _b.a, _c = _b.b, b = _c === void 0 ? 0 : _c;
+    //...
+}
+ff2({ a: "yes" }); //ok, default b = 0
+ff2(); // ok default to { a: "" }, which then default b = 0
+//ff2({}); // error, 'a' is required if you supply an argument.
+/*
+        Spread
+*/
+//Array spread
+var first2 = [1, 2];
+var second2 = [3, 4];
+var bothPlus = [0].concat(first2, second2, [5]); // values are [0, 1, 2, 3, 4, 5]
+//Object spread
+var defaults = { food: "spicy", price: "$$", ambiance: "noisy" };
+var search = __assign({}, defaults, { food: "rich" });
+var defaults1 = { food: "spicy", price: "$$", ambince: "noisy" };
+var search1 = __assign({ food: "rich" }, defaults1);
+var Cls = /** @class */ (function () {
+    function Cls() {
+        this.p = 12;
+    }
+    Cls.prototype.m = function () {
+    };
+    return Cls;
+}());
+var _Cls = new Cls();
+var clone = __assign({}, _Cls);
+clone.p; // ok
+//clone.m(); // error
